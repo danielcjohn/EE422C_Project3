d3479df16bda4b87ce51da8890e13773c163406c
Jared Cormier <Jared Cormier>
2017-02-21 17:51:59 -0600

    final assignment 3 commit

--- Main.java
+++ Main.java
@@ -1,17 +1,15 @@
 /* WORD LADDER Main.java
  * EE422C Project 3 submission by
- * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
- * <Student1 5-digit Unique No.>
- * <Student2 Name>
- * <Student2 EID>
- * <Student2 5-digit Unique No.>
- * Slip days used: <0>
- * Git URL:
+ * Jared Cormier
+ * jcc4969
+ * 16238
+ * Daniel John
+ * dcj597
+ * 16238
+ * Slip days used: 0
+ * Git URL: https://github.com/danielcjohn/EE422C_Project3
  * Spring 2017
  */
-
 
 package assignment3;
 import java.util.*;
@@ -19,9 +17,7 @@ import java.io.*;
 
 public class Main {
 	
-	public static Map<String, ArrayList<String>> AdjacencyList;
-	public static Set<String> explored = new HashSet<String>();
-	public static Node parent = null;
+	private static Map<String, ArrayList<String>> AdjacencyList;
 	
 	
 	public static void main(String[] args) throws Exception {
@@ -44,7 +40,7 @@ public class Main {
 		while(!input.contains("/quit")){ //if /quit is one of two words entered, terminate program
 			String start = input.get(0);
 			String end = input.get(1);
-			ArrayList<String> ladder = getWordLadderDFS(start, end);
+			ArrayList<String> ladder = getWordLadderBFS(start, end);
 			printLadder(ladder);
 			input = parse(kb);
 		}
@@ -73,10 +69,17 @@ public class Main {
 	public static ArrayList<String> getWordLadderDFS(String start, String end) {
 		start = start.toUpperCase();
     	end = end.toUpperCase();
+    	if(start.equals(end)){
+    		//if start and end word are same, return list with both words and print "no ladder found"
+    		ArrayList<String> dfsLadder = new ArrayList<String>();
+    		dfsLadder.add(start);
+    		dfsLadder.add(end);
+    		return dfsLadder;
+    	}
 		Node first = new Node(start, null);	//starting position
 		Set<String> visited = new HashSet<String>();
-		Node found = findDFS(first, end, visited);
-		if(!(found==null)){
+		Node found = findDFS(first, end, visited);  //call recursive function to find linked list of word ladder
+		if(!(found==null)){ //if list found, create word ladder and return
 			ArrayList<String> dfsLadder = new ArrayList<String>();
 			dfsLadder.add(end);
 			found = found.parent;
@@ -87,34 +90,15 @@ public class Main {
 			return dfsLadder;
 			}
 		else{
-			ArrayList<String> ladder = new ArrayList<String>();
-			ladder.add(start);
-			ladder.add(end);
-			return ladder;
+			//if no word ladder is found, return list with only start and end word
+			ArrayList<String> dfsLadder = new ArrayList<String>();
+			dfsLadder.add(start);
+			dfsLadder.add(end);
+			return dfsLadder;
 		}
 	}
 		
-	public static Node findDFS(Node n, String end, Set<String> visited){
-		if(n==null){
-			return null;
-		}
-		visited.add(n.word);
-		if(n.word.equals(end)){
-			return n;
-		}else{
-			ArrayList<String> dfsNeighbors = AdjacencyList.get(n.word);
-			for(String j : dfsNeighbors){
-				if(!visited.contains(j)){
-					Node current = new Node(j, n);
-					Node found = findDFS(current, end, visited);
-					if(!(found==null)){
-						return found;
-					}
-					}
-				}
-			return null;
-			}
-		}
+	
 		
 	
     public static ArrayList<String> getWordLadderBFS(String start, String end) {
@@ -122,10 +106,10 @@ public class Main {
     	end = end.toUpperCase();
     	if(start.equals(end)){
     		//if start and end word are same, return list with both words and print "no ladder found"
-    		ArrayList<String> ladder = new ArrayList<String>();
-    		ladder.add(start);
-    		ladder.add(end);
-    		return ladder;
+    		ArrayList<String> bfsLadder = new ArrayList<String>();
+    		bfsLadder.add(start);
+    		bfsLadder.add(end);
+    		return bfsLadder;
     	}
     	Set<String> visited = new HashSet<String>(); //keep track of visited words
 		Queue<Node> qe = new LinkedList<Node>(); 
@@ -134,14 +118,14 @@ public class Main {
 		while(!qe.isEmpty()){
 			Node current = qe.remove();
 			if(current.word.equals(end)){ 			//if end word found, create word ladder and return;
-				ArrayList<String> ladder = new ArrayList<String>();
-				ladder.add(current.word);
+				ArrayList<String> bfsLadder = new ArrayList<String>();
+				bfsLadder.add(current.word);
 				current = current.parent;
 				while(current != null){
-					ladder.add(current.word);
+					bfsLadder.add(current.word);
 					current = current.parent;
 				}
-				return ladder;
+				return bfsLadder;
 			}
 			ArrayList<String> neighbors = AdjacencyList.get(current.word);	
 			for(String n : neighbors){										//for each neighbor of current head
@@ -154,10 +138,10 @@ public class Main {
 		}
 		
 		//if no word ladder is found, return list with only start and end word
-		ArrayList<String> ladder = new ArrayList<String>();
-		ladder.add(start);
-		ladder.add(end);
-		return ladder;
+		ArrayList<String> bfsLadder = new ArrayList<String>();
+		bfsLadder.add(start);
+		bfsLadder.add(end);
+		return bfsLadder;
 	}
     
 	public static Set<String>  makeDictionary () {
@@ -196,7 +180,7 @@ public class Main {
 	* Each key to the Map is a word from the dictionary and each
 	* 	Map value is list of words that differ from key by one letter
 	*/
-	public static Map<String, ArrayList<String>> CreateAdjacencyList(Set<String> dict){
+	private static Map<String, ArrayList<String>> CreateAdjacencyList(Set<String> dict){
 	Map<String, ArrayList<String>> AList = new HashMap<>();
 	for(String n : dict){
 		Iterator<String> it = dict.iterator();
@@ -229,5 +213,56 @@ public class Main {
 		}
 		if(diff > 1){return false;}
 		return true;
+	}
+	
+	/*recursive function used to obtain word ladder by DFS
+	 *returns last node of linked list of connected word ladder if found
+	 *returns null node if no ladder found
+	 */
+	private static Node findDFS(Node n, String end, Set<String> visited){
+		if(n==null){   //if all possible paths have been checked and end word still not found, return null
+			return null;
+		}
+		visited.add(n.word);
+		if(n.word.equals(end)){   //when end word found, return last node of linked list of word ladder
+			return n;
+		}else{
+			ArrayList<String> dfsNeighbors = orderDFSNeighbors(AdjacencyList.get(n.word), end); //obtain neighbor nodes of current word and order them to reduce word ladder length if found
+			for(String j : dfsNeighbors){
+				if(!visited.contains(j)){
+					Node current = new Node(j, n);
+					Node found = findDFS(current, end, visited);
+					if(!(found==null)){
+						return found;
+					}
+					}
+				}
+			return null;
+			}
+		}
+	
+	/*function used to attempt to reduce length of word ladder obtained by DFS
+	 * orders neighbor nodes of a word by putting words that match 3 or more letter of the end word first in line
+	 * returns List of newly ordered words
+	 */
+	private static ArrayList<String> orderDFSNeighbors(ArrayList<String> n, String end){
+		ArrayList<String> ordered = new ArrayList<String>();
+		for(String s : n){
+			int matches = 0;
+			for(int i = 0; i<s.length(); i++){
+				if(s.charAt(i)==(end.charAt(i))){
+					matches++;
+				}
+			}
+			if(matches>=3){
+				ordered.add(s);
+			}
+		}
+		for(String s : n){
+			if(!ordered.contains(n)){
+				ordered.add(s);
+			}
+		}
+		return ordered;
 	}
 }
--- Node.java
+++ Node.java
@@ -1,3 +1,15 @@
+/* WORD LADDER Main.java
+ * EE422C Project 3 submission by
+ * Jared Cormier
+ * jcc4969
+ * 16238
+ * Daniel John
+ * dcj597
+ * 16238
+ * Slip days used: 0
+ * Git URL: https://github.com/danielcjohn/EE422C_Project3
+ * Spring 2017
+ */
 package assignment3;
 
 public class Node {
